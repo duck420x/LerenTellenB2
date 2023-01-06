514de993217b49f892145a4424b8efcf34910af0
87243 <87243@glr.nl>
2023-01-06 03:35:17 +0100

    update

--- highscores/index.php
+++ highscores/index.php
@@ -11,6 +11,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Leren Tellen | Highscores</title>
         <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/highscoresStyle.css">
     </head>
     <body>
         <!-- Navbar -->
