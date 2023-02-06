e7a36023b88b73f775662957379ac9a698db5506
87243 <87243@glr.nl>
2023-02-06 16:03:54 +0100

    Update index.php

--- game/index.php
+++ game/index.php
@@ -1,5 +1,9 @@
 <!-- Start a php session (this must be before <!DOCTYPE html>!!!) -->
 <?php
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+
     session_start();
 ?>
 
